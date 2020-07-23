b44cc458d3047f7de5ceef72c0665360536ef127
Sunil <sunil.shegu@apty.io>
2020-07-23 16:48:53 +0530

    Adds Should deploy

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const core = require('@actions/core');
 
 try {
   let branchName = '';
+  let shouldDeploy = false;
   const eventName = process.env.GITHUB_EVENT_NAME;
 
+  // Fetch Branch Name
   if (eventName === 'pull_request') {
     branchName = process.env.GITHUB_HEAD_REF;
   } else if (eventName === 'push') {
     branchName = process.env.GITHUB_REF.replace('refs/heads/', '');
   }
 
-  core.setOutput('branch_name', branchName);
+  // Validate and set branch Name
   const validBranchRegex = /(^(feature|bugfix|hotfix)\/(LSP|CB)-[0-9]{1,5}\/[0-9a-zA-Z_-]+$)|(^(development|staging|production|qa|labs)$)/;
-
   if (!validBranchRegex.test(branchName)) {
     core.setFailed(`Branch Name should be in the regex format ${validBranchRegex}`);
+  } else {
+    core.setOutput('branch_name', branchName);
   }
+
+  // Calculate should_deploy and set as output
+  const deployableBranches = ['development', 'qa', 'staging', 'production', 'labs'];
+  if(deployableBranches.includes(branchName)) {
+    shouldDeploy = true;
+  }
+  core.setOutput('should_deploy', shouldDeploy);
 } catch (error) {
   core.setFailed(error.message);
 }
