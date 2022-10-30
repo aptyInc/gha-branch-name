9ac91c25ae0c4ff0365451dfb731245c51a0b5c5
saiumesh-apty <saiumesh.dhanewar@apty.io>
2022-10-30 16:19:22 +0530

    added demo branch

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ try {
   }
 
   // Validate and set branch Name
-  const validBranchRegex = /(^(revert)-[0-9]{1,5}-(feature|bugfix|hotfix|onprem|test)\/(LSP|CB|AQRE|LP|ASE|CED|SAP|FR)-[0-9]{1,5}\/[0-9a-zA-Z_-]+$)|(^(feature|bugfix|hotfix|onprem|test)\/(LSP|CB|AQRE|LP|ASE|CED|SAP|FR)-[0-9]{1,5}\/[0-9a-zA-Z_-]+$)|(^(development|staging|production|qa|qa1|hotfix|labs|onprem|nightly)$)|((rc)-\d*.\d*.\d*)/;
+  const validBranchRegex = /(^(revert)-[0-9]{1,5}-(feature|bugfix|hotfix|onprem|test)\/(LSP|CB|AQRE|LP|ASE|CED|SAP|FR)-[0-9]{1,5}\/[0-9a-zA-Z_-]+$)|(^(feature|bugfix|hotfix|onprem|test)\/(LSP|CB|AQRE|LP|ASE|CED|SAP|FR)-[0-9]{1,5}\/[0-9a-zA-Z_-]+$)|(^(development|staging|production|qa|qa1|hotfix|labs|demo|onprem|nightly)$)|((rc)-\d*.\d*.\d*)/;
   if (!validBranchRegex.test(branchName)) {
     core.setFailed(`Branch Name should be in the regex format ${validBranchRegex}`);
   } else {
@@ -23,14 +23,14 @@ try {
   }
  
   // Calculate should_deploy and set as output
-  const deployableBranches = ['development', 'qa', 'staging', 'production', 'labs', 'qa1', 'hotfix'];
+  const deployableBranches = ['development', 'qa', 'staging', 'production', 'labs', 'qa1', 'hotfix', 'demo'];
   if(deployableBranches.includes(branchName)) {
     shouldDeploy = true;
   }
   core.setOutput('should_deploy', shouldDeploy);
 
   // Check if it's Staging or Productions or labs
-  const ProdBranches = ['staging', 'production', 'labs'];
+  const ProdBranches = ['staging', 'production', 'labs', 'demo'];
   if(ProdBranches.includes(branchName)) {
     ProdDeploy = true;
   }
